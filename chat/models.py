155ce0537f81bd1d2ce4bf1d4573e1ee6b5dc79a
faatemeh chamani <fch199674@yahoo.com>
2021-11-23 20:05:07 +0330

    models created

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Room( models.Model ):
+    room_name = models.CharField( max_length=200, help_text='name of room' )
+
+    def __str__(self):
+        return self.room_name
+
+
+class Message( models.Model ):
+    owner = models.ForeignKey( User, on_delete=models.CASCADE )
+    message_text = models.CharField( max_length=1000 )
+    date_added = models.DateTimeField( auto_now_add=True, help_text='message date and time' )
+    room = models.ForeignKey( 'Room', on_delete=models.CASCADE, null=True )
+
+    def __str__(self):
+        return self.owner.username
